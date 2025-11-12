f18d22e8ced454684cdfad4cab3d7d7f906c7aac
Johnny Jiang <130807463+Jiang-Johnny@users.noreply.github.com>
2025-11-12 03:12:54 -0500

    Create Encoder.py

---
+++ Encoder.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class ClimbEncoder(nn.Module):
+
+    def __init__(self, vocab_size):
+
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, 256)
+
+        #This is the "model" we will use for classification
+        encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=8)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=8)
+        self.fc = nn.Linear(256, 20)  
+
+    def forward(self, input):
+
+        x = self.embedding(input) 
+        x = x.transpose(0, 1) 
+
+        encoded = self.encoder(x)
+
+        pooled = encoded.mean(dim=0)  
+
+        result = self.fc(pooled)
+
+        return result, encoded
+
+    
